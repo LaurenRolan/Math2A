cf5d1194575c57e4424307448c9b96c5ccf16dcd
LaurenRolan <lauren.rolan@ecole.ensicaen.fr>
2018-12-14 16:13:27 +0100

    TP3

---
+++ PageRank/fonctions.py
@@ -0,0 +1,59 @@
+import numpy as np
+import matplotlib as mpl
+
+
+def create_mini_net_A():
+    A = np.matrix([[0, 1/3., 1/3., 1/3.],
+                   [0, 0, 1/2., 1/2.],
+                   [1, 0, 0, 0],
+                   [1/2., 0, 1/2., 0]])
+    return A
+
+def calculateM(A, alpha):
+    M = A * (1 - alpha)
+    S = np.ones((M.shape[0], M.shape[0]))
+    S = S * (alpha * 1 / M.shape[0])
+    M = M + S
+    return M
+
+def get_constants(A, gamma):
+    beta = 4 + A.shape[0] * gamma
+    mu = 1.99 / beta
+    return beta, mu
+
+def hessiene(M, gamma):
+    I = np.ones((M.shape[0], M.shape[0]))
+    hessiene = np.transpose(M - I)
+    hessiene = hessiene * (M - I)
+    hessiene += gamma * M.shape[0]
+    return hessiene
+
+def F(x, M, gamma):
+    I = np.ones((M.shape[0], M.shape[0]))
+    X = np.subtract(M, I)
+    X = np.multiply(X, x)
+    inner_X = np.sum(X*X)
+    inner_X = (inner_X ** 2) / 2
+
+    inner_Y = np.sum(x) - 1
+    inner_Y = (inner_Y ** 2) / 2
+    inner_Y *= gamma
+
+    return inner_X + inner_Y
+
+def get_score_vector(M):
+    n = M.shape[0]
+    v, w = np.linalg.eig(M)
+    i = np.where(np.abs(v-1) < 1e-5)
+    score = w[:, i].reshape((n, 1))
+    score /= score.sum()
+    return score.real
+
+A = create_mini_net_A()
+M = calculateM(A, 0.15)
+gamma = float (1) / M.shape[0]
+mu, beta = get_constants(A, gamma)
+x_etoile = get_score_vector(M)
+
+print(hessiene(M, gamma))
+print(F(x_etoile, M, gamma))
